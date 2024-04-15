b471d2186be66c3a7bfe5391e47f3f9a7da7ac43
Haijian (Will) Wu <WillWuHJ@outlook.com>
2024-04-15 00:13:01 -0700

    round 2

--- algorithm.py
+++ algorithm.py
@@ -1,7 +1,7 @@
 from typing import List
 import numpy as np
 import json
-from datamodel import Listing, Observation, Order, OrderDepth, ProsperityEncoder, Symbol, Trade, TradingState
+from datamodel import ConversionObservation, Listing, Observation, Order, OrderDepth, ProsperityEncoder, Symbol, Trade, TradingState
 from typing import Any
 import copy
 import collections
@@ -172,25 +172,28 @@ class Trader:
 
     POSITION_LIMIT = {
         'AMETHYSTS': 20,
-        'STARFRUIT': 20
+        'STARFRUIT': 20,
+        'ORCHIDS': 100,
     }
 
     position = {
         'AMETHYSTS': 0,
-        'STARFRUIT': 0
+        'STARFRUIT': 0,
+        'ORCHIDS': 0,
     
     }
 
     round1_products = ['AMETHYSTS', 'STARFRUIT']
+    round2_products = ['ORCHIDS']
 
     starfruit_coefs = {
         # "vwap": 1,
         # "vwap_change": -0.25076721339661984,
         # "imbalance_volume_signed": -0.17395201352494163,
-        "best_mean": 0.9970645785765161,
-        "best_mean_-1": 0.0007666006190972529,
-        "best_mean_-2": 0.0003870777600799704,
-        "best_mean_-3": 0.00011440834143569041,
+"best_mean": 1.0012252201206766,
+"best_mean_-1": -0.00011613099877059405,
+"best_mean_-2": 0.0003941628661304437,
+"best_mean_-3": -0.00023525537272049696,
         # "best_mean_MA_3": -0.4522749419653542,
         # "best_mean_MA_10": 0.00012514476494644223,
     }
@@ -208,8 +211,15 @@ class Trader:
         # "best_mean_MA_10": None,
     }
 
+
+    cumulative_sunlight = 0
+    past_humidity = []
+    humidity_change = []
+    
+
+
     def predict_starfruit(self):
-        res = 8.376824143833801
+        res = -6.523275510305211
 
         for key, val in self.starfruit_cache.items():
             res += val * self.starfruit_coefs[key]
@@ -337,6 +347,147 @@ class Trader:
 
             return orders
         
+    def calc_round2_order(self, product: str, order_depth: OrderDepth, 
+                          fair_bid: float, fair_ask: float, orchids_observation: ConversionObservation, timestamp: int = 0) -> List[Order]:
+        orders: list[Order] = []
+        conversions: int = 0
+        
+        LIMIT = self.POSITION_LIMIT[product]
+
+        order_sell = collections.OrderedDict(sorted(order_depth.sell_orders.items()))
+        order_buy = collections.OrderedDict(sorted(order_depth.buy_orders.items(), reverse=True))
+
+        
+        real_bid = orchids_observation.bidPrice - orchids_observation.exportTariff - orchids_observation.transportFees - 0.1
+        real_ask = orchids_observation.askPrice + orchids_observation.importTariff + orchids_observation.transportFees
+        
+        print(f"real bid {real_bid}  real ask {real_ask}  current_pos: {self.position[product]}")
+
+        print(order_sell, order_buy)
+
+        curr_pos = self.position[product]
+        '''
+        Implement Arbitrage just by import/export First
+        '''
+        for ask, vol in order_sell.items():
+            if (ask < real_bid):
+                order_for = min(-vol, LIMIT-curr_pos)
+                curr_pos += order_for # the order is balanced by conversion
+                orders.append(Order(product, ask, order_for))
+                orders.append(Order(product, int(real_bid), -order_for))
+                conversions -= order_for
+        
+        for bid, vol in order_buy.items():
+            if (bid > real_ask):
+                order_for = max(-vol, -LIMIT-curr_pos)
+                curr_pos += order_for # the order is balanced by conversion
+                orders.append(Order(product, bid, order_for))
+                orders.append(Order(product, int(real_ask + 0.9), -order_for))
+                conversions -= order_for
+
+
+        sell_vol, best_sell_price = values_extract(order_sell)
+        buy_vol, best_buy_price = values_extract(order_buy, 1)
+
+        best_sell_price = list(order_sell.items())[0][0]
+        best_buy_price = list(order_buy.items())[0][0]
+
+        spread = (best_sell_price - best_buy_price) // 2
+        if (best_sell_price - spread) > real_ask: # if the local ask - 3 > real_ask in south, we can buy real ask and sell ask - 3 on local
+            order_for = -int(0.9 * (LIMIT - curr_pos)) # don't care for position, we can balance by conversion
+            orders.append(Order(product, best_sell_price - spread, order_for))
+            orders.append(Order(product, int(real_ask + 0.9), -order_for))
+            conversions -= order_for
+            print(f" Want to sell on local with the price {best_sell_price - spread} in {order_for} shares and would buy with the price {int(real_ask + 0.9)} in {-order_for} shares")
+        
+        if (best_buy_price + spread) < real_bid: # if the local ask - 3 > real_ask in south, we can buy real ask and sell ask - 3 on local
+            order_for = int(0.6 * (LIMIT - curr_pos)) # don't care for position, we can balance by conversion
+            orders.append(Order(product, best_buy_price + spread, order_for))
+            orders.append(Order(product, int(real_bid), -order_for))
+            conversions -= order_for
+            print(f" Want to buy on local with the price {best_buy_price + spread} in {order_for} shares and would buy with the price {int(real_bid)} in {-order_for} shares")
+
+        '''
+        if (best_sell_price + best_buy_price) / 2 > real_ask:
+            order_for = max(-min(sell_vol, buy_vol), -LIMIT - curr_pos)
+            curr_pos += order_for
+            orders.append(Order(product, int((best_sell_price + best_buy_price) / 2), -order_for))
+            orders.append(Order(product, int(real_ask + 0.9), order_for))
+            print(f" Want to sell on local with the price {int((best_sell_price + best_buy_price) / 2)} in {-order_for} shares and would buy with the price {int(real_ask + 1)} in {order_for} shares")
+            conversions += order_for
+        '''
+        
+        '''
+        Arbitrage from sunlight & humidity 
+        
+
+        sunlight = orchids_observation.sunlight
+        humidity = orchids_observation.humidity
+
+        self.cumulative_sunlight += sunlight * 12 / 10000 # 10000 datapoints per trading day; per trading day 12 hours
+        remaining_time = (10000 - timestamp // 100) * 12 / 10000
+        # expected sunlight: 7*2500, or 420 minutes sunlight with 2500 / hour
+        expected_sunlight = 7 * 2500
+        forecast_sunlight = self.cumulative_sunlight + remaining_time * sunlight
+
+        sunlight_pos = 0
+        if forecast_sunlight < expected_sunlight: # the production decreasing, long now
+            minutes_less = (expected_sunlight - forecast_sunlight) / 2500 * 60
+            sunlight_pos += minutes_less / 10 * 4
+        
+        humidity_pos = 0
+        if not self.past_humidity:
+            self.past_humidity.append(humidity)
+        elif len(self.past_humidity) < 200:
+            self.past_humidity.append(humidity)
+            self.humidity_change.append(humidity - self.past_humidity[-1])
+        else:
+            self.past_humidity.pop(0)
+            self.humidity_change.pop(0)
+            self.past_humidity.append(humidity)
+            self.humidity_change.append(humidity - self.past_humidity[-1])
+
+        if 60 <= humidity <= 80:
+            if sunlight_pos != 0:
+                order_for = min(-sell_vol, sunlight_pos, LIMIT - curr_pos)
+                orders.append(Order(product, best_sell_price + 1, order_for))
+                curr_pos += order_for
+                print(f"Orders pure sunlight {best_sell_price + 1}  {order_for}")
+        elif humidity < 60:
+            humidity_diff = 60 - humidity
+            production_decrease = humidity_diff * 0.4
+            if self.past_humidity == 200:
+                if sum(self.humidity_change) > 0: # go back to the range, we should short
+                    humidity_pos -= production_decrease
+                elif sum(self.humidity_change) < 0: # still out of the range, we should long
+                    humidity_pos += production_decrease
+        elif humidity > 80:
+            humidity_diff = humidity - 80
+            production_decrease = humidity_diff * 0.4
+            if self.past_humidity == 200:
+                if sum(self.humidity_change) < 0: # go back to the range, we should short
+                    humidity_pos -= production_decrease
+                elif sum(self.humidity_change) > 0: # still out of the range, we should long
+                    humidity_pos += production_decrease
+            
+                
+        change_pct = (100 + sunlight_pos) * (100 + humidity_pos) / 10000
+        fair_price = (best_sell_price + best_buy_price) // 2
+
+        if change_pct > 1:
+            order_for = LIMIT - curr_pos   
+            orders.append(Order(product, fair_price, order_for))
+            print(f"Orders combined {fair_price}  {order_for}")
+            curr_pos += order_for
+        elif change_pct < 1:
+            order_for = -LIMIT - curr_pos
+            orders.append(Order(product, fair_price, order_for))
+            print(f"Orders combined {fair_price}  {order_for}")
+            curr_pos += order_for
+'''
+        return orders, conversions
+
+
 
     
     def run(self, state: TradingState):
@@ -344,26 +495,18 @@ class Trader:
 
         result = {
             'AMETHYSTS': [],
-            'STARFRUIT': []
+            'STARFRUIT': [],
+            'ORCHIDS':[],
         }
 
 
         for key, val in state.position.items():
             self.position[key] = val
 
-        timestamp = state.timestamp
+        timestamp = state.timestamp // 100
 
+        # ROUND 1
         starfruit_lb, starfruit_ub = -1e9, 1e9
-
-        '''
-        "vwap": None,
-        "vwap_change": None,
-        "imbalance_volume_signed": None,
-        "best_mean": None,
-        "best_mean_-1": None,
-        "best_mean_-2": None,
-        '''
-        
         '''
         starfruit_pre_vwap = self.starfruit_cache["vwap"] 
         self.starfruit_cache["vwap"] = calc_vwap(order_depth = state.order_depths['STARFRUIT'])
@@ -397,12 +540,14 @@ class Trader:
         
         acceptable_bids = {
             'AMETHYSTS': 10000,
-            'STARFRUIT': starfruit_lb
+            'STARFRUIT': starfruit_lb,
+            'ORCHIDS': 0,
         }
 
         acceptable_asks = {
             'AMETHYSTS': 10000,
-            'STARFRUIT': starfruit_ub
+            'STARFRUIT': starfruit_ub,
+            'ORCHIDS': 0,
         }
 
         for product in self.round1_products:
@@ -410,10 +555,22 @@ class Trader:
             orders = self.calc_round1_order(product, order_depth, acceptable_bids[product], acceptable_asks[product],)
             if orders:
                 result[product] += orders
+
+        # ROUND 2
+        conversions = 0
+        orchids_observation = state.observations.conversionObservations['ORCHIDS']
+
+
+        for product in self.round2_products:
+            order_depth: OrderDepth = state.order_depths[product]
+            print(f"observation -- bid:{orchids_observation.bidPrice},  ask:{orchids_observation.askPrice}, export:{orchids_observation.exportTariff},  import:{orchids_observation.importTariff}, transport;{orchids_observation.transportFees}")
+            orders, conversions = self.calc_round2_order(product, order_depth, acceptable_bids[product], acceptable_asks[product], orchids_observation, timestamp)
+            if orders:
+                result[product] += orders
     
         traderData = "" # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
         
-        conversions = 1
+        
 
         # self.logger.flush(state, result, conversions, traderData)
 
